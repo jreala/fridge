d63c32c67fceb57572a50ef0290a28030b9a936e
Jason Eala <11766330+jreala@users.noreply.github.com>
2018-10-13 09:55:38 -0700

    Add date picker and base for filter

---
+++ src/components/datePicker.vue
@@ -0,0 +1,15 @@
+<template>
+    <datepicker :placeholder="hintText"></datepicker>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker';
+export default {
+    props: {
+        hintText: String
+    },
+    components: {
+        Datepicker
+    }
+}
+</script>
