da13615716ba650dcfa0bdaa9cee5c9c762d478f
Jason Eala <11766330+jreala@users.noreply.github.com>
2018-10-14 13:36:32 -0700

    Pass data

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,29 @@
         <header-component header="Big Fridge" blurb="Interview Project by Jason Eala"></header-component>
         <filter-component></filter-component>
         <br/>
-        <table-component></table-component>
+        <table-component caption="Quantity By Purchase Date" :dataset="quantityByDate"></table-component>
         <br/>
-        <table-component></table-component>
+        <table-component caption="Purchased After Expiration" :dataset="purchasedAfterExpiration"></table-component>
         <br/>
-        <table-component></table-component>
+        <table-component caption="All Data" :dataset="allData"></table-component>
+        <br/>
     </div>
 </template>
 
 <script>
-import HeaderComponent from './components/header.vue';
-import FilterComponent from './components/filter.vue';
-import TableComponent from './components/table.vue';
+import HeaderComponent from "./components/header.vue";
+import FilterComponent from "./components/filter.vue";
+import TableComponent from "./components/table.vue";
+import Dataset from "./js/dataset";
 
 export default {
   name: "app",
   data() {
     return {
-      message: "Big Fridge"
+      message: "Big Fridge",
+      allData: Dataset.get(),
+      quantityByDate: Dataset.getQuantityByDate(),
+      purchasedAfterExpiration: Dataset.getPurchasedAfterExpiration()
     };
   },
   components: {
