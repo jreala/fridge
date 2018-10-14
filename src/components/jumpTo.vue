5f02f4192f1c7c344eff40a6acde847b6110019a
Jason Eala <11766330+jreala@users.noreply.github.com>
2018-10-14 14:28:31 -0700

    Formatting

--- src/components/jumpTo.vue
+++ src/components/jumpTo.vue
@@ -2,7 +2,7 @@
     <b-input-group prepend="Jump To Page...">
         <b-form-input 
             type="number" 
-            v-on:input="$emit('page-changed', page)" 
+            :input="$emit('page-changed', page)"
             v-model.number="page" 
             placeholder='Page...'
         >
@@ -12,16 +12,16 @@
 
 <script>
 export default {
-    props: ['currentPage'],
-    data() {
-        return {
-            page: this.currentPage
-        }
-    },
-    watch: {
-        currentPage(page) {
-            this.page = page;
-        }
+  props: ["currentPage"],
+  data() {
+    return {
+      page: this.currentPage
+    };
+  },
+  watch: {
+    currentPage(page) {
+      this.page = page;
     }
+  }
 };
 </script>
